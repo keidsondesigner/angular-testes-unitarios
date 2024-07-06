e115362cbffa166faeede6c3e3346581709853fd
Keidson Roby <keidsondesigner@gmail.com>
2024-07-05 21:28:15 -0400

    feat: Suite de teste configurando TestBed

--- src/app/concepts-tests/concepts-tests.component.spec.ts
+++ src/app/concepts-tests/concepts-tests.component.spec.ts
@@ -3,18 +3,18 @@ import { ConceptsTestsComponent } from './concepts-tests.component';
 
 describe('ConceptsTestsComponent', () => {
 
-  let component: ConceptsTestsComponent;
-  let fixture: ComponentFixture<ConceptsTestsComponent>
+  let component: ConceptsTestsComponent; // criado a variável e Tipando o componente com o tipo ConceptsTestsComponent
+  let fixture: ComponentFixture<ConceptsTestsComponent> // criado a variável e Tipando o componente com o tipo ComponentFixture<ConceptsTestsComponent>
 
   beforeEach(() => {
 
-    TestBed.configureTestingModule({
+    TestBed.configureTestingModule({ // configureTestingModule é um auxiliar que ajuda a configura o modulo para testing.
       declarations: [ConceptsTestsComponent],
     }).compileComponents()
 
 
-    fixture = TestBed.createComponent(ConceptsTestsComponent)
-    component = fixture.componentInstance
+    fixture = TestBed.createComponent(ConceptsTestsComponent) // injecta o componente atraves do createComponent()
+    component = fixture.componentInstance // instância o componente, com isso tenho acesso as atributos, propriedades, metodos e elementos do DOM.
     fixture.detectChanges();
   })
 
