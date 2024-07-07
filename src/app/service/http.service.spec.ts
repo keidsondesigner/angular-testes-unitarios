d54f3fd39bb63fe26ef0c9492e98bf6adfb939ee
Keidson Roby <keidsondesigner@gmail.com>
2024-07-07 13:23:35 -0400

    feat: Teste requisição GET por Id

--- src/app/service/http.service.spec.ts
+++ src/app/service/http.service.spec.ts
@@ -4,8 +4,8 @@ import { TestBed } from '@angular/core/testing';
 import { HttpService } from './http.service';
 
 describe('HttpService', () => {
-  let service: HttpService;
-  let htppTestingController: HttpTestingController;
+  let service: HttpService; // Simula meu Service
+  let htppTestingController: HttpTestingController; // Simula chamadas HTTP
   let url: any;
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -20,4 +20,23 @@ describe('HttpService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('Deve realizar chamada GET por id', () => {
+    const id = 3;
+    const MOCK_USER = {
+      id: 3,
+      name: "Danilo",
+      email: "danilo@gmail.com",
+      age: "30",
+    }
+
+    service.getUsersById(id).subscribe(res => {
+      expect(res).toEqual(MOCK_USER);
+    });
+
+    const req = htppTestingController.expectOne(`${url}/users/${id}`);
+
+    expect(req.request.method).toBe('GET');
+    expect(req.request.url).toBe(`${url}/users/${id}`);
+    req.flush(MOCK_USER); // flush() Simula a resposta do retorno da chamada GET
+  });
 });
