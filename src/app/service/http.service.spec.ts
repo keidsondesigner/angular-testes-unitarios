422945a0607f3ead6897547e906837e8025d8044
Keidson Roby <keidsondesigner@gmail.com>
2024-07-07 16:29:45 -0400

    feat: Teste com Headers na requisição

--- src/app/service/http.service.spec.ts
+++ src/app/service/http.service.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { TestBed } from '@angular/core/testing';
 
 import { HttpService } from './http.service';
+import { HttpHeaders } from '@angular/common/http';
 
 describe('HttpService', () => {
   let service: HttpService; // Simula meu Service
@@ -139,15 +140,30 @@ describe('HttpService', () => {
 
   it('Deve realizar chamada DELETE para exculír um usuário', () => {
     const id = 2;
+    const response = {}
 
     service.deleteUser(id).subscribe(res => {
-      expect(res).toBe({});
+      expect(res).toBe(response);
     });
 
     const req = htppTestingController.expectOne(`${url}/users/${id}`);
 
     expect(req.request.method).toBe('DELETE');
     expect(req.request.url).toBe(`${url}/users/${id}`);
-    req.flush({}); // flush() Simula a resposta do retorno da chamada POST
+    req.flush(response); // flush() Simula a resposta do retorno da chamada POST
+  });
+
+  it('Deve conter os headers na requisição', () => {
+    const token = 'wa45a5a45a5a5a4a5a4a5a55a5a5a5a5a5a5a';
+
+    service.getUserWithHeaders().subscribe();
+
+    const req = htppTestingController.expectOne(`${url}/users`);
+
+    expect(req.request.headers.has('Authorization')).toBeTruthy();
+    expect(req.request.headers.get('Authorization')).toEqual(`Bearer ${token}`);
+    expect(req.request.headers.has('content-type')).toBeTruthy();
+    expect(req.request.headers.get('content-type')).toEqual('application/json');
+    // req.flush([]);
   });
 });
