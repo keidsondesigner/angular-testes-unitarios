2072da95910f697e3ceb86ec71927513ff365b4a
Keidson Roby <keidsondesigner@gmail.com>
2024-07-08 10:01:46 -0400

    feat: Testando @Input  e  @Output

--- src/app/input-output/input-output.component.spec.ts
+++ src/app/input-output/input-output.component.spec.ts
@@ -21,4 +21,25 @@ describe('InputOutputComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('Deve receber os dados do usuário @Input', () => {
+    // dados passado no component pai
+    const user = {name: 'Danilo', email: 'danilodev.silva@gmail.com', password: '091011'};
+
+    component.user = user;
+
+    expect(component.user).toBe(user);
+  });
+
+  it('Deve emitir mensagem quando o botão for clicado @Output', () => {
+    // click do botão é passado do filho pro pai
+    // escultando os eventos emitidos pelo userMessage
+    const emitMessage = spyOn(component.userMessage, 'emit');
+
+    let button: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    button.click();
+
+    // ao clicar no botão, espero que o evento emitMessage, seja chamado(emitido);
+    expect(emitMessage).toHaveBeenCalled();
+  });
+
 });
