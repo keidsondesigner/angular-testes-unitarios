f67226b63b7fdc16a613d19bf8ba0688b1805d44
Keidson Roby <keidsondesigner@gmail.com>
2024-07-08 23:30:03 -0400

    feat: teste Diretivas

--- src/app/hover-focus.directive.ts
+++ src/app/hover-focus.directive.ts
@@ -14,5 +14,4 @@ export class HoverFocusDirective {
   @HostListener('mouseout') onLeave() {
     this.backgroundColor = 'inherit';
   }
-
 }
--- src/app/testing-directive/testing-directive.component.spec.ts
+++ src/app/testing-directive/testing-directive.component.spec.ts
@@ -9,8 +9,10 @@ describe('TestingDirectiveComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TestingDirectiveComponent,
-      HoverFocusDirective ]
+      declarations: [
+        TestingDirectiveComponent,
+        HoverFocusDirective
+      ]
     })
     .compileComponents();
 
@@ -23,4 +25,25 @@ describe('TestingDirectiveComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('Deve alterar background quando passar o mouse sobre o título.', () => {
+    const title: HTMLHeadingElement =  fixture.nativeElement.querySelector('h1');
+
+    // Simula o evento de passar o mouse sobre o título
+    triggerMouseEvent(title, 'mouseover');
+    // title.dispatchEvent(new MouseEvent('mouseover'));
+    fixture.detectChanges();
+    expect(title.style.backgroundColor).toBe('blue')
+
+    // Simula o evento de remover o mouse do título
+    triggerMouseEvent(title, 'mouseout');
+    // title.dispatchEvent(new MouseEvent('mouseout'));
+    fixture.detectChanges();
+    expect(title.style.backgroundColor).toBe('inherit')
+  });
+
+  function triggerMouseEvent(element: HTMLElement, eventType: string) {
+    const event = new Event(eventType);
+    element.dispatchEvent(event);
+    fixture.detectChanges();
+  }
 });
